aea0d54f34c6233d61d9937b0623a270b74b5996
Khunkurisu <creative.3d@live.com>
2024-10-03 06:49:42 -0500

    ifEquals helper for handlebars
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/cosmere-rpg-unofficial.mjs
+++ module/cosmere-rpg-unofficial.mjs
@@ -59,6 +59,9 @@ Handlebars.registerHelper('toLowerCase', function (str) {
 Handlebars.registerHelper('toUpperCase', function (str) {
 	return str.toUpperCase();
 });
+Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+	return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
 
 /* -------------------------------------------- */
 /*  Ready Hook                                  */
