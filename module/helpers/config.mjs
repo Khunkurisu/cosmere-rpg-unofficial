d5e70d3c042b697ec8bb57cdf3b1d72fdf2fa517
Khunkurisu <creative.3d@live.com>
2024-10-05 09:54:04 -0500

    reorganizing skill localization by branch
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -39,24 +39,30 @@ COSMERE_UNOFFICIAL.attributeAbbreviations = {
 	presence: 'COSMERE_UNOFFICIAL.Attribute.Presence.abbr'
 };
 COSMERE_UNOFFICIAL.skills = {
-	athletics: 'COSMERE_UNOFFICIAL.Skill.Athletics.long',
-	agility: 'COSMERE_UNOFFICIAL.Skill.Agility.long',
-	heavy_weapons: 'COSMERE_UNOFFICIAL.Skill.HeavyWeapons.long',
-	light_weapons: 'COSMERE_UNOFFICIAL.Skill.LightWeapons.long',
-	stealth: 'COSMERE_UNOFFICIAL.Skill.Stealth.long',
-	thievery: 'COSMERE_UNOFFICIAL.Skill.Thievery.long',
-	crafting: 'COSMERE_UNOFFICIAL.Skill.Crafting.long',
-	deduction: 'COSMERE_UNOFFICIAL.Skill.Deduction.long',
-	discipline: 'COSMERE_UNOFFICIAL.Skill.Discipline.long',
-	intimidation: 'COSMERE_UNOFFICIAL.Skill.Intimidation.long',
-	lore: 'COSMERE_UNOFFICIAL.Skill.Lore.long',
-	medicine: 'COSMERE_UNOFFICIAL.Skill.Medicine.long',
-	deception: 'COSMERE_UNOFFICIAL.Skill.Deception.long',
-	insight: 'COSMERE_UNOFFICIAL.Skill.Insight.long',
-	leadership: 'COSMERE_UNOFFICIAL.Skill.Leadership.long',
-	perception: 'COSMERE_UNOFFICIAL.Skill.Perception.long',
-	persuasion: 'COSMERE_UNOFFICIAL.Skill.Persuasion.long',
-	survival: 'COSMERE_UNOFFICIAL.Skill.Survival.long',
+	physical: {
+		athletics: 'COSMERE_UNOFFICIAL.Skill.Athletics.long',
+		agility: 'COSMERE_UNOFFICIAL.Skill.Agility.long',
+		heavy_weapons: 'COSMERE_UNOFFICIAL.Skill.HeavyWeapons.long',
+		light_weapons: 'COSMERE_UNOFFICIAL.Skill.LightWeapons.long',
+		stealth: 'COSMERE_UNOFFICIAL.Skill.Stealth.long',
+		thievery: 'COSMERE_UNOFFICIAL.Skill.Thievery.long'
+	},
+	cognitive: {
+		crafting: 'COSMERE_UNOFFICIAL.Skill.Crafting.long',
+		deduction: 'COSMERE_UNOFFICIAL.Skill.Deduction.long',
+		discipline: 'COSMERE_UNOFFICIAL.Skill.Discipline.long',
+		intimidation: 'COSMERE_UNOFFICIAL.Skill.Intimidation.long',
+		lore: 'COSMERE_UNOFFICIAL.Skill.Lore.long',
+		medicine: 'COSMERE_UNOFFICIAL.Skill.Medicine.long'
+	},
+	spiritual: {
+		deception: 'COSMERE_UNOFFICIAL.Skill.Deception.long',
+		insight: 'COSMERE_UNOFFICIAL.Skill.Insight.long',
+		leadership: 'COSMERE_UNOFFICIAL.Skill.Leadership.long',
+		perception: 'COSMERE_UNOFFICIAL.Skill.Perception.long',
+		persuasion: 'COSMERE_UNOFFICIAL.Skill.Persuasion.long',
+		survival: 'COSMERE_UNOFFICIAL.Skill.Survival.long'
+	},
 	expertise: 'COSMERE_UNOFFICIAL.Expertise.label',
 	expertises_new: 'COSMERE_UNOFFICIAL.Expertise.create',
 	expertises_remove: 'COSMERE_UNOFFICIAL.Expertise.delete',
@@ -64,24 +70,30 @@ COSMERE_UNOFFICIAL.skills = {
 };
 
 COSMERE_UNOFFICIAL.skillAbbreviations = {
-	athletics: 'COSMERE_UNOFFICIAL.Skill.Athletics.abbr',
-	agility: 'COSMERE_UNOFFICIAL.Skill.Agility.abbr',
-	heavy_weapons: 'COSMERE_UNOFFICIAL.Skill.HeavyWeapons.abbr',
-	light_weapons: 'COSMERE_UNOFFICIAL.Skill.LightWeapons.abbr',
-	stealth: 'COSMERE_UNOFFICIAL.Skill.Stealth.abbr',
-	thievery: 'COSMERE_UNOFFICIAL.Skill.Thievery.abbr',
-	crafting: 'COSMERE_UNOFFICIAL.Skill.Crafting.abbr',
-	deduction: 'COSMERE_UNOFFICIAL.Skill.Deduction.abbr',
-	discipline: 'COSMERE_UNOFFICIAL.Skill.Discipline.abbr',
-	intimidation: 'COSMERE_UNOFFICIAL.Skill.Intimidation.abbr',
-	lore: 'COSMERE_UNOFFICIAL.Skill.Lore.abbr',
-	medicine: 'COSMERE_UNOFFICIAL.Skill.Medicine.abbr',
-	deception: 'COSMERE_UNOFFICIAL.Skill.Deception.abbr',
-	insight: 'COSMERE_UNOFFICIAL.Skill.Insight.abbr',
-	leadership: 'COSMERE_UNOFFICIAL.Skill.Leadership.abbr',
-	perception: 'COSMERE_UNOFFICIAL.Skill.Perception.abbr',
-	persuasion: 'COSMERE_UNOFFICIAL.Skill.Persuasion.abbr',
-	survival: 'COSMERE_UNOFFICIAL.Skill.Survival.abbr'
+	physical: {
+		athletics: 'COSMERE_UNOFFICIAL.Skill.Athletics.abbr',
+		agility: 'COSMERE_UNOFFICIAL.Skill.Agility.abbr',
+		heavy_weapons: 'COSMERE_UNOFFICIAL.Skill.HeavyWeapons.abbr',
+		light_weapons: 'COSMERE_UNOFFICIAL.Skill.LightWeapons.abbr',
+		stealth: 'COSMERE_UNOFFICIAL.Skill.Stealth.abbr',
+		thievery: 'COSMERE_UNOFFICIAL.Skill.Thievery.abbr'
+	},
+	cognitive: {
+		crafting: 'COSMERE_UNOFFICIAL.Skill.Crafting.abbr',
+		deduction: 'COSMERE_UNOFFICIAL.Skill.Deduction.abbr',
+		discipline: 'COSMERE_UNOFFICIAL.Skill.Discipline.abbr',
+		intimidation: 'COSMERE_UNOFFICIAL.Skill.Intimidation.abbr',
+		lore: 'COSMERE_UNOFFICIAL.Skill.Lore.abbr',
+		medicine: 'COSMERE_UNOFFICIAL.Skill.Medicine.abbr'
+	},
+	spiritual: {
+		deception: 'COSMERE_UNOFFICIAL.Skill.Deception.abbr',
+		insight: 'COSMERE_UNOFFICIAL.Skill.Insight.abbr',
+		leadership: 'COSMERE_UNOFFICIAL.Skill.Leadership.abbr',
+		perception: 'COSMERE_UNOFFICIAL.Skill.Perception.abbr',
+		persuasion: 'COSMERE_UNOFFICIAL.Skill.Persuasion.abbr',
+		survival: 'COSMERE_UNOFFICIAL.Skill.Survival.abbr'
+	},
 };
 
 COSMERE_UNOFFICIAL.defenses = {
