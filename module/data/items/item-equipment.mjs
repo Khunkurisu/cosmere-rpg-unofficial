6f5c5f457e3ab1b763f609ecd661404896fcb288
Khunkurisu <creative.3d@live.com>
2024-10-07 05:57:47 -0500

    new equipped values and helper getters
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/data/items/item-equipment.mjs
+++ module/data/items/item-equipment.mjs
@@ -6,11 +6,27 @@ export default class CosmereUnofficialEquipment extends CosmereUnofficialItemBas
 		const requiredInteger = { required: true, nullable: false, integer: true };
 		const schema = super.defineSchema();
 
-		schema.quantity = new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 });
+		schema.quantity = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
 		schema.weight = new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0 });
 		schema.price = new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0 });
-		schema.equipped = new fields.BooleanField({ required: true, nullable: false, initial: false });
+		schema.equipped = new fields.SchemaField({
+			isEquipped: new fields.BooleanField({ required: true, nullable: false, initial: false }),
+			slot: new fields.StringField({ initial: "worn" }),
+			hands: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 })
+		});
 
 		return schema;
+	}
+
+	get totalWeight() {
+		return this.quantity * this.weight;
+	}
+
+	get totalPrice() {
+		return this.quantity * this.price;
+	}
+
+	get isEquipped() {
+		return this.equipped.isEquipped;
 	}
 }
