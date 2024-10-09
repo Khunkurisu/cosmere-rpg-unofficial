103468db56831304d32d1038d9bdd05f32991fb0
Khunkurisu <creative.3d@live.com>
2024-10-09 13:35:06 -0500

    displayWeight field for showing total weight values
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/data/items/item-equipment.mjs
+++ module/data/items/item-equipment.mjs
@@ -8,6 +8,7 @@ export default class CosmereUnofficialEquipment extends CosmereUnofficialItemBas
 
 		schema.quantity = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
 		schema.weight = new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0 });
+		schema.displayWeight = new fields.StringField({ required: true, blank: true });
 		schema.price = new fields.NumberField({ required: true, nullable: false, initial: 0, min: 0 });
 		schema.equipped = new fields.SchemaField({
 			isEquipped: new fields.BooleanField({ required: true, nullable: false, initial: false }),
