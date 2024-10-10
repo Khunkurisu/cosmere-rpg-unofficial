25692217f3e2a8a8ebe2f4a6cd58b72548ced203
Khunkurisu <creative.3d@live.com>
2024-10-10 13:10:41 -0500

    default status of effects to 'active'
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/data/items/item-effect.mjs
+++ module/data/items/item-effect.mjs
@@ -6,7 +6,7 @@ export default class CosmereUnofficialEffect extends CosmereUnofficialItemBase {
 		const requiredInteger = { required: true, nullable: false, integer: true };
 		const schema = super.defineSchema();
 
-		schema.status = new fields.StringField({ initial: "talent" });
+		schema.status = new fields.StringField({ initial: "active" });
 
 		schema.duration = new fields.SchemaField({
 			value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
