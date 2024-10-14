82fff7289f9167c7ee834def9c91f6cddaa9810e
Khunkurisu <creative.3d@live.com>
2024-10-14 10:16:40 -0500

    fixed errors from merge
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -27,13 +27,6 @@ export class CosmereUnofficialItem extends Item {
 	getRollData() {
 		// Starts off by populating the roll data with a shallow copy of `this.system`
 		const rollData = { ...this.system };
-	/**
-	 * Prepare a data object which defines the data schema used by dice roll commands against this Item
-	 * @override
-	 */
-	getRollData() {
-		// Starts off by populating the roll data with a shallow copy of `this.system`
-		const rollData = { ...this.system };
 
 		// Quit early if there's no parent actor
 		if (!this.actor) return rollData;
@@ -54,8 +47,6 @@ export class CosmereUnofficialItem extends Item {
 
 		return rollData;
 	}
-		return rollData;
-	}
 
 	/**
 	 * Handle clickable rolls.
@@ -64,18 +55,7 @@ export class CosmereUnofficialItem extends Item {
 	 */
 	async roll(event) {
 		const item = this;
-	/**
-	 * Handle clickable rolls.
-	 * @param {Event} event   The originating click event
-	 * @private
-	 */
-	async roll(event) {
-		const item = this;
 
-		// Initialize chat data.
-		const speaker = ChatMessage.getSpeaker({ actor: this.actor });
-		const rollMode = game.settings.get('core', 'rollMode');
-		const label = `[${item.type}] ${item.name}`;
 		// Initialize chat data.
 		const speaker = ChatMessage.getSpeaker({ actor: this.actor });
 		const rollMode = game.settings.get('core', 'rollMode');
@@ -94,31 +74,32 @@ export class CosmereUnofficialItem extends Item {
 		else {
 			// Retrieve roll data.
 			const rollData = this.getRollData();
-		// If there's no roll data, send a chat message.
-		if (!this.system.formula) {
-			ChatMessage.create({
-				speaker: speaker,
-				rollMode: rollMode,
-				flavor: label,
-				content: item.system.description ?? '',
-			});
-		}
-		// Otherwise, create a roll and send a chat message from it.
-		else {
-			// Retrieve roll data.
-			const rollData = this.getRollData();
+			// If there's no roll data, send a chat message.
+			if (!this.system.formula) {
+				ChatMessage.create({
+					speaker: speaker,
+					rollMode: rollMode,
+					flavor: label,
+					content: item.system.description ?? '',
+				});
+			}
+			// Otherwise, create a roll and send a chat message from it.
+			else {
+				// Retrieve roll data.
+				const rollData = this.getRollData();
 
-			// Invoke the roll and submit it to chat.
-			const roll = new Roll(rollData.formula, rollData);
-			// If you need to store the value first, uncomment the next line.
-			// const result = await roll.evaluate();
-			roll.toMessage({
-				speaker: speaker,
-				rollMode: rollMode,
-				flavor: label,
-				content: item.system.description ?? '',
-			});
-			return roll;
+				// Invoke the roll and submit it to chat.
+				const roll = new Roll(rollData.formula, rollData);
+				// If you need to store the value first, uncomment the next line.
+				// const result = await roll.evaluate();
+				roll.toMessage({
+					speaker: speaker,
+					rollMode: rollMode,
+					flavor: label,
+					content: item.system.description ?? '',
+				});
+				return roll;
+			}
 		}
 	}
 }
