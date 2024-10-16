9b283a956e7437b64a78aac6484544203cfcffb0
Khunkurisu <creative.3d@live.com>
2024-10-16 11:07:39 -0500

    add getter for character tier
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -373,6 +373,16 @@ export class CosmereUnofficialActor extends Actor {
 		});
 	}
 
+	get tier() {
+		const system = this.system;
+		switch (system.level) {
+			case 1: case 2: case 3: case 4: case 5: return 1;
+			case 6: case 7: case 8: case 9: case 10: return 2;
+			case 11: case 12: case 13: case 14: case 15: return 3;
+			case 16: case 17: case 18: case 19: case 20: return 4;
+		}
+	}
+
 	/**
 	 * Prepare NPC type specific data.
 	 */
