73a18604feba80e808e24effda688b986ea79c59
Khunkurisu <creative.3d@live.com>
2024-10-18 18:31:29 -0500

    double check actor speeds are proper...
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -251,8 +251,14 @@ export default class CosmereUnofficialActorBase extends CosmereUnofficialDataMod
 				source.expertise = expertises;
 			}
 		}
-
+		if (!isNumeric(source.movement.ground) || isNaN(source.movement.ground)) {
+			source.movement.ground = 20;
+		}
 		return source;
 	}
-
+}
+function isNumeric(str) {
+	if (typeof str != "string") return false // we only process strings!
+	return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+		!isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
