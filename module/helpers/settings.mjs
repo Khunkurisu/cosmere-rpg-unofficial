0b6e6f38d7688cf1eaa28c5fffd17d22d42f54e7
Khunkurisu <creative.3d@live.com>
2024-10-18 02:48:31 -0500

    updated hint to reflect the fact there's more theme options than light and dark now
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/helpers/settings.mjs
+++ module/helpers/settings.mjs
@@ -1,7 +1,7 @@
 export async function registerSettings() {
 	await game.settings.register('cosmere-rpg-unofficial', 'colorTheme', {
 		name: 'Color Theme',
-		hint: 'Sets whether to have a dark background with light text or vice versa.',
+		hint: 'Sets visual theme of application windows.',
 		scope: 'client',
 		config: true,
 		type: String,
