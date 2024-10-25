d4cfc60603178940b57aece331bc46b9b7236148
Khunkurisu <creative.3d@live.com>
2024-10-25 16:35:06 -0500

    fixed having deleted schema from base-item datamodel like an idiot
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/data/base-item.mjs
+++ module/data/base-item.mjs
@@ -3,6 +3,7 @@ import CosmereUnofficialDataModel from "./base-model.mjs";
 export default class CosmereUnofficialItemBase extends CosmereUnofficialDataModel {
 	static defineSchema() {
 		const fields = foundry.data.fields;
+		const schema = {};
 
 		schema.description = new fields.StringField({ required: true, blank: true });
 		schema.showDetails = new fields.BooleanField({ required: true, initial: false });
